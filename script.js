b563543e5fc924dbf6d0738782e4cad4aede32ac
Renja <renjaschmakeit@gmail.com>
2023-04-12 01:31:29 +0200

    added event listener für task edit. lots of problems. not working.

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ addButton.addEventListener("click", function () {
   const todayList = document.querySelector("#todayList");
   todayList.appendChild(newTask);
 });
+
+
+/* Edit Function */
+const testItem = document.querySelector('.item2');
+const listItemText = document.querySelectorAll('.listItemTxt');
+function taskEdit (e) {
+    const input = document.createElement("input");
+    const testTextGet = testItem.innerHTML;
+    input.type = "text";
+    input.value = testTextGet;
+    testItem.parentNode.replaceChild(input, testTextGet);
+    console.log(testTextGet);
+}
+console.log(listItemText);
+testItem.addEventListener("click", taskEdit);
